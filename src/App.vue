f5538c41d1d6b00db84a28184808c195c3bc66db
pbh914 <52387364+ArkBB@users.noreply.github.com>
2025-05-27 17:26:58 +0900

    init
    처음 화면 완성

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <HeaderComponent/>
+    <v-main>
+      <router-view>
+
+      </router-view>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import HeaderComponent from './components/HeaderComponent.vue'
+export default {
+  name: 'App',
+  components: {
+    HeaderComponent
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-app-bar app dark>
+    <v-container>
+      <v-row align="center">
+        <v-col class="d-flex justify-start">
+            <v-btn :to="{path: '/member/list'}">회원목록</v-btn>
+            <v-btn :to="{path: '/groupchatting/list'}">채팅방목록</v-btn>
+        </v-col>
+        <v-col class="text-center">
+            <v-btn :to="{path: '/'}"> chat서비스</v-btn>
+        </v-col>
+        <v-col class="d-flex justify-end">
+          <v-btn :to="{path: '/member/list'}" v-if="isLogin">MyChatPage</v-btn>
+          <v-btn :to="{path: '/member/list'}" v-if="!isLogin">회원가입</v-btn>
+          <v-btn :to="{path: '/member/list'}" v-if="!isLogin">로그인</v-btn>
+          <v-btn v-if="isLogin" @click="doLogout">로그아웃</v-btn>
+        </v-col>
+
+      </v-row>
+    </v-container>
+  </v-app-bar>
+</template>
+<script>
+export default{
+  created() {
+    const token = localStorage.getItem("token");
+    if(token){
+      this.isLogin = true;
+    }
+
+    }
+  ,
+  data(){
+    return {
+        isLogin: false,
+    }
+
+  },methods: {
+      doLogout(){
+        localStorage.clear();
+        window.location.reload();
+      }
+  }
+}
+</script>
