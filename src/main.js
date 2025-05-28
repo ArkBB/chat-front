dc6e094f8899fd9df33ffe38380a869321fe1f0d
pbh914 <52387364+ArkBB@users.noreply.github.com>
2025-05-28 14:25:58 +0900

    feat : 기본 웹소켓 채팅 테스트 화면

--- src/main.js
+++ src/main.js
@@ -2,9 +2,25 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from '@/router/index.js'
-//import axios from 'axios'
+import axios from 'axios'
 
 const app = createApp(App);
+
+axios.interceptors.request.use(
+    config => {
+        const token = localStorage.getItem("token");
+        if(token){
+            config.headers['Authorization'] = `Bearer ${token}`
+        }
+        return config;
+    },
+    error => {
+        //인터셉터를 무시하고 사용자의 본래요청인 화면을 라우팅
+        return Promise.reject(error);
+    }
+    //로그인이 안된 경우 오류 터짐. 로그인 하라고 안내 표시.
+
+)
 
 app.use(router);
 app.use(vuetify);
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import {createRouter, createWebHashHistory} from "vue-router"
 import MemberCreate from "@/views/MemberCreate.vue"
 import LoginPage from "@/views/LoginPage.vue";
+import MemberList from "@/views/MemberList.vue";
+import SimpleWebsocket from "@/views/SimpleWebsocket.vue";
 
 const routes = [
     {
@@ -12,6 +14,16 @@ const routes = [
         path:'/member/login',
         name: 'LoginPage',
         component: LoginPage
+    },
+    {
+        path:'/member/list',
+        name: 'MemberList',
+        component: MemberList
+    },
+    {
+        path:'/simple/chat',
+        name: 'SimpleWebsocket',
+        component: SimpleWebsocket
     }
 ];
 
